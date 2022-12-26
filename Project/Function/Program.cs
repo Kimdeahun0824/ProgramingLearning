38066e14884bb148be640e4b65d3a957f579b988
kimdeahun0824 <garamei72@gmail.com>
2022-12-26 16:00:51 +0900

    feat : 22.12.16 수업내용 및 과제 업로드

---
+++ Project/Function/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] starArray = new string[5, 5];
+
+            // 별찍기 직각 삼각형 했었던거
+            // 별을 초기화 하는 코드
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = y; x < 5; x++)
+                {
+                    starArray[y, x] = "*";
+                }
+            }   // loop : 별을 배열에 초기화 하는 루프
+
+            // 별을 출력하는 코드
+            for (int y = 0; y < 5; y++)
+            {
+                for (int x = y; x < 5; x++)
+                {
+                    if (starArray[y, x].Equals("*"))
+                    {
+                        Console.Write($"{starArray[y, x]} ");
+                    }
+                }
+                Console.WriteLine();
+            }   // loop : 배열에 담긴 별을 출력하는 루프
+        }
+    }
+}
---
+++ Project/Function/Task.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function
+{
+    internal class Task
+    {
+        /**
+         * 5x5 보드 ( 사이즈 줘도 ok)
+         * 빈 곳 중에 아무곳이나 사람( 이모티콘 또는 옷)을 초기화해서
+         * w, a, s, d 입력 받아서 빈 곳을 자유롭게 이동하는 프로그램 작성.
+         * - 조건 사람은 빈 곳을 다닐 수 있음
+         */
+
+        static int FIELDMAX_X = 10;
+        static int FIELDMAX_Y = 10;
+        static string[,] field = new string[FIELDMAX_X, FIELDMAX_Y];
+        static int playerX = 3;
+        static int playerY = 3;
+
+        static ConsoleKey Input;
+        static bool Escape = false;
+
+        static void Main(string[] args)
+        {
+            fieldInit();
+            while (!Escape)
+            {
+                fieldDraw();
+                move();
+                Console.Clear();
+            }
+
+        }
+
+        static ConsoleKey getInput()
+        {
+            ConsoleKeyInfo Input;
+            Input = Console.ReadKey(true);
+            return Input.Key;
+        }
+        static void fieldInit()
+        {
+
+            for (int y = 0; y < FIELDMAX_Y; y++)
+            {
+                for (int x = 0; x < FIELDMAX_X; x++)
+                {
+                    if (y.Equals(0) || y.Equals(FIELDMAX_Y - 1) || x.Equals(0) || x.Equals(FIELDMAX_X - 1))
+                    {
+                        field[y, x] = "■";
+                    }
+                    else
+                    {
+                        field[y, x] = ". ";
+                    }
+                }
+            }
+        }
+        static void fieldDraw()
+        {
+            for (int y = 0; y < FIELDMAX_Y; y++)
+            {
+                for (int x = 0; x < FIELDMAX_X; x++)
+                {
+                    if (playerX.Equals(x) && playerY.Equals(y))
+                    {
+                        Console.Write("♥");
+                    }
+                    else
+                    {
+                        Console.Write($"{field[y, x]}");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static void move()
+        {
+            Input = getInput();
+            switch (Input)
+            {
+                case ConsoleKey.W:
+                    if (playerY < 2)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        playerY--;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    if (FIELDMAX_Y - 2 <= playerY)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        playerY++;
+                    }
+                    break;
+                case ConsoleKey.A:
+                    if (playerX < 2)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        playerX--;
+                    }
+                    break;
+                case ConsoleKey.D:
+                    if (FIELDMAX_X - 2 <= playerX)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        playerX++;
+                    }
+                    break;
+                case ConsoleKey.Escape:
+                    Escape = true;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
